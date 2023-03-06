1da6e5e267a926966f677c3715be27cb23084511
Khavushka <khavushka@gmail.com>
2023-03-06 10:35:09 +0100

    Running the automation script 'python file_management.py'

--- automating_file_management.py
+++ automating_file_management.py
@@ -2,3 +2,15 @@
 # Intsalling necessary libraries
 # to get started, you will need to install two libraries: os & shutil
 # Writing the automation script
+
+import os
+import shutil
+
+source_folder = 'path/folder'
+destination_folder = 'path/destination/folder'
+
+for filename in os.listdir(source_folder):
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        source = os.path.join(source_folder, filename)
+    destination = os.path.join(destination_folder, filename)
+    shutil.move(source, destination)
