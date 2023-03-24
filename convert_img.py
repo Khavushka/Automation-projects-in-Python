f7337f37ee28d226f3720b11566dc3dca11a7f5d
Khavushka <khavushka@gmail.com>
2023-03-24 12:37:46 +0100

    convert image to jpg

--- convert_img.py
+++ convert_img.py
@@ -1,2 +1,7 @@
-import os import
-import img2pdf
+from fpdf import FPDF
+Pdf = FPDF()
+list_of_images = ["eagle.png", "second.jpg","third.jpg"]
+for i in list_of_images:
+   Pdf.add_page()
+   Pdf.image(i,x,y,w,h)
+   Pdf.output("out.pdf", "F")
